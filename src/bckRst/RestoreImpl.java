6a2409e2b677f2758122a3cf039253d1ee53e0a1
Stefano Falzaresi <stefano.falzaresi2@studio.unibo.it>
2015-02-06 22:15:39 +0100

    Fixed package name

---
+++ src/bckRst/RestoreImpl.java
@@ -0,0 +1,18 @@
+package bckRst;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+	/**
+	 * 
+	 * @author Stefano Falzaresi Stefano.Falzaresi2@studio.unibo.it
+	 */
+public class RestoreImpl implements Restore {
+
+		// guarda qui http://www.mrwebmaster.it/java/xml-java-esempio-parsing-jaxp_7488_2.html
+		public boolean checkFilePresence() {
+			return false;
+		}
+	
+	
+
+}
