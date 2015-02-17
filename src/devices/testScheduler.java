06ed36a0ab37108a2a73d17408876baf3237cd3d
Marco Versari <marco.versari@studio.unibo.it>
2015-02-17 20:54:03 +0100

    first proof of the scheduling

---
+++ src/devices/testScheduler.java
@@ -0,0 +1,14 @@
+package devices;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testScheduler {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
