3aa4761dac422a62f01ceea2ea0f655d61a3ba71
Stefano Falzaresi <stefano.falzaresi2@studio.unibo.it>
2015-03-26 00:14:52 +0100

    edit on mainclass

--- src/domo/general/MainClass.java
+++ src/domo/general/MainClass.java
@@ -1,11 +1,15 @@
 package domo.general;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import domo.GUI.*;
 import domo.bckRst.*;
 
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome!");
 		Restore res = new RestoreImpl();
@@ -19,7 +23,20 @@ public class MainClass {
 
 		//usato per OSX (barra menu a schermo)
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		GUIFlatImpl t = new GUIFlatImpl("Domo");
+		//GUIFlatImpl t = new GUIFlatImpl("Domo");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Scegli se vuoi creare un nuovo appartamento o se vuoi caricare da file"C);
+		System.out.println("Digita N per nuovo appartamento o C per caricare");
+		String s = br.readLine();
+		if(s.equals("C")){
+			System.out.println("HAi scelto di caricare");
+		}
+		else if (s.equals("N")){
+			System.out.println("HAi scelto nuovo appartamento");
+			
+			Flat fl = new FlatImpl("appartamento");
+			fl.addRoom(new RoomImpl("Cucina"));
+		}
 
 	}
 
