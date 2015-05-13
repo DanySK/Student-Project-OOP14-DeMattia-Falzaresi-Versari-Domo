7f7fb59f109bfbaffb56d51fa30619852c74f800
Stefano Falzaresi <stefano.falzaresi2@studio.unibo.it>
2015-05-13 12:54:11 +0200

    refactor

---
+++ src/domo/bckrst/Crypter.java
@@ -0,0 +1,21 @@
+package domo.bckrst;
+
+	/**
+	 * 
+	 * @author Stefano Falzaresi Stefano.Falzaresi2@studio.unibo.it
+	 *
+	 * This is a useful class for manage encryption and decryption
+	 */
+public interface Crypter {
+
+	/**
+	 * With this class is possible encrypt the given encrypted file.
+	 * @throws Exception Encryption exception
+	 */
+	void doEncryption() throws Exception;
+	/**
+	 * Whit this class is possible to decrypt a previously encrypted file.
+	 * @throws Exception decryption exception
+	 */
+	void doDecryption() throws Exception;
+}
