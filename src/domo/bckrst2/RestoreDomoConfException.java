fb4f5777ec935f7d37df73954253262735d66d5f
Stefano Falzaresi <stefano.falzaresi2@studio.unibo.it>
2015-05-13 10:39:28 +0200

    ok

---
+++ src/domo/bckrst2/RestoreDomoConfException.java
@@ -0,0 +1,25 @@
+package domo.bckrst2;
+/**
+ * 
+ * @author Stefano Falzaresi Stefano.Falzaresi2@studio.unibo.it
+ * 
+ * This is the class for restore related exception
+ *
+ */
+public class RestoreDomoConfException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private String err;
+	/**
+	 * The constructor of the exception for showing backup related errors. 
+	 * @param e the error message to showe 
+	 */
+	public RestoreDomoConfException(final String e) {
+		this.err = e;
+	}
+	
+	@Override
+	public String toString() {
+		return "Error during restore procedure: " + err;
+	}
+}
