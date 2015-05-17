747ac962cc9bd2b828f68320c7728e213f492017
Stefano Falzaresi <stefano.falzaresi2@studio.unibo.it>
2015-05-17 20:10:55 +0200

    added InputOutput

---
+++ src/domo/educational/gui/GuiTestClass.java
@@ -0,0 +1,17 @@
+package domo.educational.gui;
+
+public class GuiTestClass {
+
+	/**
+	 * 
+	 * @author Stefano Falzaresi Stefano.Falzaresi2@studio.unibo.it
+	 * 
+	 * This is a generic class for GUI tests
+	 *
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
